df78325b1faf4d65bf160867356b2118c49d937b
Gorgutc <axximus@gmail.com>
2019-06-15 19:18:24 +0300

    Delete settings_bot.py

--- settings_bot.py
+++
@@ -1,4 +0,0 @@
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
-
-API_KEY = '714756531:AAHYo30Twqv3ed38LvMW7WazXL-H19Y5N0c'
